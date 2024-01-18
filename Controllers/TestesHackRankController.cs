9bf142c45d020df6fd14f42857d9043d3443c1eb
Gabriel Spiandorello <gabrielspian@gmail.com>
2024-01-17 23:30:43 -0300

    Add project files.

---
+++ Controllers/TestesHackRankController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace testHackRank.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestesHackRankController : ControllerBase
+    {
+
+        private readonly ILogger<TestesHackRankController> _logger;
+
+        public TestesHackRankController(ILogger<TestesHackRankController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "PrimeiroTesteHackRank")]
+        public string SumNumbers(int a, int b)
+        {
+            if (CheckNumberValue(a, b))
+                return (a + b).ToString();
+            else
+                return "Reveja os numeros adicionados";
+        }
+
+
+        [HttpGet(Name = "Simple Array Sum")]
+        public string SimpleArraySum()
+        {
+            return "";
+        }
+
+
+        #region Helpers
+        protected bool CheckNumberValue(int a, int b)
+        {
+            return a >= 1 && b <= 1000 ? true : false;           
+        }
+
+        #endregion
+    }
+}
